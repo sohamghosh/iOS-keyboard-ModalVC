e33dffb99f88563065fe202b3b2c2195597154f2
Soham Ghosh <sohamghosh@gmail.com>
2013-12-06 16:55:36 +0530

    Initial Commit

---
+++ Keyboard/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Keyboard
+//
+//  Created by Soham on 06/12/13.
+//  Copyright (c) 2013 TW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Keyboard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Keyboard
+//
+//  Created by Soham on 06/12/13.
+//  Copyright (c) 2013 TW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
